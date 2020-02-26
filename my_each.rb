563dc2e34166e9c533210646ad90d9f616c84c7e
Daniel Kim <daniel.kim52@gmail.com>
2020-02-26 12:00:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,11 @@
 def my_each(collection) # put argument(s) here
-  collection.each do |i|
-    puts i
-  end
+ i = 0
+ while i < collection.length
+   yield array[i]
+   i = 1 + i
+ end
+end
+
+my_each(collection) do |name|
+  puts "#{name}"
 end
